f42f723daae0b65264784e7fe66adfc869828a51
Finn <121757305+Arifindepayjr1@users.noreply.github.com>
2024-12-19 09:02:46 +0700

    Create main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Flowers;      
+class Users;
+class Cart;
+class Linklist;
+
+
+
+int main()
+{
+
+    std::cout << "\n\n";
+    std::cout << "\n\n Start Program \n\n";
+
+
+
+
+
+
+
+
+
+
+    std::cout << "\n\n";
+
+    return 0;
+}
